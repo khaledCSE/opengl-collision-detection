8065ad1b1200b09a914be52d4e776ff31f5bf23d
khaledCSE <khaledcse10@gmail.com>
2023-10-10 09:05:50 +0600

    Sphere moves downwards

--- sphereJK_OO_basic.cpp
+++ sphereJK_OO_basic.cpp
@@ -4,6 +4,10 @@
 class SphereDemo : public Application
 {
   int radius = 10;
+  int x = 0;
+  int y = 0;
+  int xStep = 2;
+  int yStep = 2;
 
 public:
   virtual void display();
@@ -13,6 +17,8 @@ public:
 void SphereDemo::display()
 {
   Application::display();
+
+  glTranslatef(x, y, 0.0f);
   glColor3ub(255, 0, 0);
   glutSolidSphere(radius, 30, 30);
   glutSwapBuffers();
@@ -20,6 +26,8 @@ void SphereDemo::display()
 
 void SphereDemo::update()
 {
+  x += xStep;
+  y += yStep;
   if (radius <= Application::height && radius <= Application::width)
     radius++;
   else
