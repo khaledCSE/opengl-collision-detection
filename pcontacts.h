299d706fd6cc91b79f576b727e663d522e7fd908
khaledCSE <khaledcse10@gmail.com>
2023-11-01 17:12:12 +0600

    Minor fix

--- pcontacts.h
+++ pcontacts.h
@@ -8,117 +8,114 @@
 
 #include "particle.h"
 
+class ParticleContactResolver;
 
-    class ParticleContactResolver;
+/**
+ * A Contact represents two objects in contact (in this case
+ * ParticleContact representing two Particles).
+ */
+class ParticleContact
+{
+    /**
+     * The contact resolver object needs access into the contacts to
+     * set and effect the contact.
+     */
+    friend ParticleContactResolver;
+
+public:
+    /**
+     * Holds the particles that are involved in the contact. The
+     * second of these can be NULL, for contacts with the scenery.
+     */
+    Particle *particle[2];
 
     /**
-     * A Contact represents two objects in contact (in this case
-     * ParticleContact representing two Particles). 
+     * Holds the normal restitution coefficient at the contact.
      */
-    class ParticleContact
-    {
-        /**
-         * The contact resolver object needs access into the contacts to
-         * set and effect the contact.
-         */
-        friend ParticleContactResolver;
-
-    public:
-        /**
-         * Holds the particles that are involved in the contact. The
-         * second of these can be NULL, for contacts with the scenery.
-         */
-        Particle* particle[2];
-
-        /**
-         * Holds the normal restitution coefficient at the contact.
-         */
-        float restitution;
-
-        /**
-         * Holds the direction of the contact in world coordinates.
-         */
-        Vector2 contactNormal;
-
-        /**
-         * Holds the depth of penetration at the contact.
-         */
-        float penetration;
-
-
-    protected:
-        /**
-         * Resolves this contact, for both velocity and interpenetration.
-         */
-        void resolve(float duration);
-
-        /**
-         * Calculates the separating velocity at this contact.
-         */
-        float calculateSeparatingVelocity() const;
-
-    private:
-        /**
-         * Handles the impulse calculations for this collision.
-         */
-        void resolveVelocity(float duration);
-
-    };
+    float restitution;
 
     /**
-     * The contact resolution routine for particle contacts. One
-     * resolver instance can be shared for the whole simulation.
+     * Holds the direction of the contact in world coordinates.
      */
-    class ParticleContactResolver
-    {
-    protected:
-        /**
-         * Holds the number of iterations allowed.
-         */
-        unsigned iterations;
-
-        /**
-         * This is a performance tracking value - we keep a record
-         * of the actual number of iterations used.
-         */
-        unsigned iterationsUsed;
-
-    public:
-        /**
-         * Creates a new contact resolver.
-         */
-        ParticleContactResolver(unsigned iterations);
-
-        /**
-         * Sets the number of iterations that can be used.
-         */
-        void setIterations(unsigned iterations);
-
-        /**
-         * Resolves a set of particle contacts for both penetration
-         * and velocity.
-         *
-        */
-        void resolveContacts(ParticleContact *contactArray,
-            unsigned numContacts,
-            float duration);
-    };
+    Vector2 contactNormal;
 
     /**
-     * This is the basic polymorphic interface for contact generators
-     * applying to particles.
+     * Holds the depth of penetration at the contact.
      */
-    class ParticleContactGenerator
-    {
-    public:
-        /**
-         * Fills the given contact structure with the generated
-         * contact. 
-         */
-        virtual unsigned addContact(ParticleContact *contact,
-                                    unsigned limit) const = 0;
-    };
+    float penetration;
 
-	
+protected:
+    /**
+     * Resolves this contact, for both velocity and interpenetration.
+     */
+    void resolve(float duration);
+
+    /**
+     * Calculates the separating velocity at this contact.
+     */
+    float calculateSeparatingVelocity() const;
+
+private:
+    /**
+     * Handles the impulse calculations for this collision.
+     */
+    void resolveVelocity(float duration);
+};
+
+/**
+ * The contact resolution routine for particle contacts. One
+ * resolver instance can be shared for the whole simulation.
+ */
+class ParticleContactResolver
+{
+protected:
+    /**
+     * Holds the number of iterations allowed.
+     */
+    unsigned iterations;
+
+    /**
+     * This is a performance tracking value - we keep a record
+     * of the actual number of iterations used.
+     */
+    unsigned iterationsUsed;
+
+public:
+    /**
+     * Creates a new contact resolver.
+     */
+    ParticleContactResolver(unsigned iterations);
+
+    /**
+     * Sets the number of iterations that can be used.
+     */
+    void setIterations(unsigned iterations);
+
+    /**
+     * Resolves a set of particle contacts for both penetration
+     * and velocity.
+     *
+     */
+    void resolveContacts(ParticleContact *contactArray,
+                         unsigned numContacts,
+                         float duration);
+};
+
+/**
+ * This is the basic polymorphic interface for contact generators
+ * applying to particles.
+ */
+class ParticleContactGenerator
+{
+public:
+    Particle *particle;
+    float coeff_of_restitution;
+    /**
+     * Fills the given contact structure with the generated
+     * contact.
+     */
+    virtual unsigned addContact(ParticleContact *contact,
+                                unsigned limit) const = 0;
+};
 
 #endif // CONTACTS_H
